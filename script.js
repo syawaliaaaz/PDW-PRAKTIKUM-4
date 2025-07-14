3aa09bb735c619693bcbbeff6f811e5d8fa44b98
syawaliaaaz <helfinasyawaliaaa@gmail.com>
2025-07-14 15:50:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Fungsi untuk menampilkan tanggal
+function tampilkanTanggal() {
+  let sekarang = new Date();
+  document.getElementById("tanggal").innerHTML = "Tanggal dan waktu sekarang: " + sekarang;
+  alert("Tanggal berhasil ditampilkan!");
+}
+
+// Fungsi alert kreatif sesuai kreasi
+function alertKreatif() {
+  alert("nyari apa sih! ");
+}
+
+// Fungsi alert lain (misalnya ucapan)
+function ucapan() {
+  alert("selamat menyelesaikan sems 4 ya wkwkwk!");
+}
